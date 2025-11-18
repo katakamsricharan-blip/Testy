26a37b6dcf4529be8b8de615368c471ec6075b31
KATAKAM SRICHARAN <katakamsricharan@KATAKAMs-MacBook-Air.local>
2025-11-18 18:01:46 +0530

    Newfile

---
+++ SampleTest.java
@@ -0,0 +1,12 @@
+package TestNg;
+	import org.testng.annotations.Test;
+
+	public class SampleTest {
+
+	    @Test
+	    public void testMethod() {
+	        System.out.println("TestNG is working!");
+	    }
+	}
+
+
